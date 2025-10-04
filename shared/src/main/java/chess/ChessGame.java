65c6dbb852d1746415ea5e1ec69866e23c0d93e0
cal-jamo <calvin.jameson02@gmail.com>
2025-10-03 20:48:01 -0600

    Use teh helper recently pushed for checkmate check

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -175,7 +175,10 @@ public class ChessGame {
      * @return True if the specified team is in checkmate
      */
     public boolean isInCheckmate(TeamColor teamColor) {
-        throw new RuntimeException("Not implemented");
+        if (!isInCheck(teamColor)) {
+            return false;
+        }
+        return !hasAnyValidMoves(teamColor);
     }
 
     /**
