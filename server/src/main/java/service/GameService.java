ae8ddf83a23530c5e6d8e52acde239d562647530
cal-jamo <calvin.jameson02@gmail.com>
2025-10-20 12:52:35 -0600

    Adding the method signatures for creating a game and listing the games

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -1,5 +1,9 @@
 package service;
 import dataaccess.*;
+import model.GameData;
+
+import javax.xml.crypto.Data;
+import java.util.Collection;
 
 
 public class GameService {
@@ -10,5 +14,16 @@ public class GameService {
         this.authDAO = authDAO;
         this.gameDAO = gameDAO;
     }
+
+    // take authToken and gameName as parameters, and create and store the game data
+    public int createGame(String authToken, String gameName) throws DataAccessException {
+
+    }
+
+    // take authToken as a param and if auth token is valid return the list of games stored in memory
+    public Collection<GameData> listGames(String authToken) throws DataAccessException {
+
+    }
+
 
 }
