00bb5b2c3148d2068de88cfbf9def728136e1ddd
cal-jamo <calvin.jameson02@gmail.com>
2025-10-20 12:42:18 -0600

    Structure to begin working on Game service

---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,14 @@
+package service;
+import dataaccess.*;
+
+
+public class GameService {
+    private final AuthDAO authDAO;
+    private final GameDAO gameDAO;
+
+    public GameService(AuthDAO authDAO, GameDAO gameDAO) {
+        this.authDAO = authDAO;
+        this.gameDAO = gameDAO;
+    }
+
+}
