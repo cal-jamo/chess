7212908237210e5e11d30d13961cf0f1cf7b1082
cal-jamo <calvin.jameson02@gmail.com>
2025-10-30 22:08:50 -0600

    Set up my create auth function to insert an auth session into the auth table, and making get connection available to DBAuthDAO

--- server/src/main/java/dataaccess/auth/DBAuthDAO.java
+++ server/src/main/java/dataaccess/auth/DBAuthDAO.java
@@ -1,6 +1,9 @@
 package dataaccess.auth;
 import dataaccess.DataAccessException;
 import model.AuthData;
+import java.sql.SQLException;
+import java.util.UUID;
+import dataaccess.DatabaseManager;
 
 public class DBAuthDAO implements AuthDAO {
 
@@ -9,7 +12,19 @@ public class DBAuthDAO implements AuthDAO {
 
     @Override
     public AuthData createAuth(String username) throws DataAccessException {
-        return null;
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, username);
+        var sqlQuery = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var statement = connection.prepareStatement(sqlQuery)) {
+                statement.setString(1, authData.authToken());
+                statement.setString(2, authData.username());
+                statement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new DataAccessException("Failed to create auth: " + e.getMessage());
+        }
+        return authData;
     }
 
     @Override
