d55221c176f08fd7ab3e474e726b649c7d49e07e
cal-jamo <calvin.jameson02@gmail.com>
2025-10-30 21:44:55 -0600

    creating game and tips(For use later) table with the correct schema and calling the init in main. Successful

--- server/src/main/java/dataaccess/DBInitialization.java
+++ server/src/main/java/dataaccess/DBInitialization.java
@@ -19,13 +19,35 @@ public class DBInitialization {
                     statement.executeUpdate();
                 }
                 var initUserTable = """            
-                    CREATE TABLE IF NOT EXISTS user (
+                    CREATE TABLE IF NOT EXISTS users (
                         username VARCHAR(255) NOT NULL,
                         password VARCHAR(255) NOT NULL,
                         email VARCHAR(255),
                         PRIMARY KEY (username)
                     )""";
                 try (var statement = connection.prepareStatement(initUserTable)) {
+                    statement.executeUpdate();
+                }
+                var initGamesTable = """            
+                    CREATE TABLE IF NOT EXISTS games (
+                        gameID INTEGER NOT NULL,
+                        whiteUsername VARCHAR(255) NOT NULL,
+                        blackUsername VARCHAR(255) NOT NULL,
+                        gameName VARCHAR(255) NOT NULL,
+                        chessGame TEXT NOT NULL,
+                        PRIMARY KEY (gameID)
+                    )""";
+                try (var statement = connection.prepareStatement(initGamesTable)) {
+                    statement.executeUpdate();
+                }
+                // This isn't required, but I thought it would be fun to put on my chess application
+                var initTipsTable = """            
+                    CREATE TABLE IF NOT EXISTS facts (
+                        id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                        category VARCHAR(50) NOT NULL,
+                        factText TEXT NOT NULL
+                    )""";
+                try (var statement = connection.prepareStatement(initTipsTable)) {
                     statement.executeUpdate();
                 }
             }
