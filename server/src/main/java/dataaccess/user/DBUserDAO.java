5631c61af38ad70f1c2c957f18d29abf80b0f629
cal-jamo <calvin.jameson02@gmail.com>
2025-10-30 23:09:25 -0600

    Passing tests for creating, getting, and clearing users for the tests I have written

--- server/src/main/java/dataaccess/user/DBUserDAO.java
+++ server/src/main/java/dataaccess/user/DBUserDAO.java
@@ -1,21 +1,56 @@
 package dataaccess.user;
 import dataaccess.DataAccessException;
+import dataaccess.DatabaseManager;
+import model.AuthData;
 import model.UserData;
+
+import java.sql.SQLException;
 
 public class DBUserDAO implements UserDAO {
 
     @Override
     public void insertUser(UserData user) throws DataAccessException {
-
+        var userQuery = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var statement = connection.prepareStatement(userQuery)) {
+                statement.setString(1, user.username());
+                statement.setString(2, user.password());
+                statement.setString(3, user.email());
+                statement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new DataAccessException("Failed to insert user: " + e.getMessage());
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        var userQuery = "SELECT * FROM users WHERE username = ?";
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var statement = connection.prepareStatement(userQuery)) {
+                statement.setString(1, username);
+                try (var resultSet = statement.executeQuery()) {
+                    if (resultSet.next()) {
+                        return new UserData(resultSet.getString("username"), resultSet.getString("password"), resultSet.getString("email"));
+                    } else {
+                        return null;
+                    }
+                }
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new DataAccessException("Failed to get user: " + e.getMessage());
+        }
     }
 
     @Override
     public void clear() throws DataAccessException {
-
+        var userQuery = "DELETE FROM users";
+        try (var connection = DatabaseManager.getConnection()) {
+            try (var statement = connection.prepareStatement(userQuery)) {
+                statement.executeUpdate();
+            }
+        } catch (SQLException | DataAccessException e) {
+            throw new DataAccessException("Failed to clear users table: " + e.getMessage());
+        }
     }
 }
