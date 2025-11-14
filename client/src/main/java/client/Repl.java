4c9cb01aaee4b67ee5043e2a8982e97b6dbffce0
cal-jamo <calvin.jameson02@gmail.com>
2025-11-13 17:16:10 -0700

    Setting up my repl to start writin that boy up

---
+++ client/src/main/java/client/Repl.java
@@ -0,0 +1,41 @@
+package client;
+import java.util.Scanner;
+
+public class Repl {
+    private boolean isLoggedIn = false;
+
+    public static void main(String[] args) {
+        System.out.println("♕ Welcome to Chess. Type 'help' to get started.");
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+
+            if (false /* I will use isLoggedIn later */) {
+                System.out.print("[LOGGED_IN] >>> ");
+            } else {
+                System.out.print("[LOGGED_OUT] >>> ");
+            }
+
+            String line = scanner.nextLine();
+            String[] tokens = line.split(" ");
+            String command = tokens[0].toLowerCase();
+
+            switch (command) {
+                case "help":
+                    System.out.println("You typed 'help'.");
+                    break;
+                case "quit":
+                    System.out.println("Exiting Chess.");
+                    return;
+                case "login":
+                    System.out.println("You typed 'login'.");
+                    break;
+                case "register":
+                    System.out.println("You typed 'register'.");
+                    break;
+                default:
+                    System.out.println("Unknown command. Type 'help' for options.");
+                    break;
+            }
+        }
+    }
+}
