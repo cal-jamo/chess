36d8d4385e99bda13015cd0c0b7530590b2d064d
cal-jamo <calvin.jameson02@gmail.com>
2025-12-02 21:41:39 -0700

    Set up my web socket handler and havent implemented it just have place holders for now

---
+++ server/src/main/java/webSocket/WebSocketHandler.java
@@ -0,0 +1,29 @@
+package webSocket;
+import com.google.gson.Gson;
+import io.javalin.websocket.WsMessageContext;
+import websocket.commands.UserGameCommand;
+import websocket.commands.MakeMoveCommand;
+
+public class WebSocketHandler {
+    private final Gson gson = new Gson();
+    public void handleMessage(WsMessageContext ctx) {
+        String message = ctx.message();
+        UserGameCommand command = gson.fromJson(message, UserGameCommand.class);
+
+        switch (command.getCommandType()) {
+            case CONNECT -> {
+                System.out.println("Connect command received");
+            }
+            case MAKE_MOVE -> {
+                MakeMoveCommand moveCommand = gson.fromJson(message, MakeMoveCommand.class);
+                System.out.println("Make Move command received");
+            }
+            case LEAVE -> {
+                System.out.println("Leave command received");
+            }
+            case RESIGN -> {
+                System.out.println("Resign command received");
+            }
+        }
+    }
+}
