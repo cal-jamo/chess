daceb4b5764719041d47e3374a0cf31340927aeb
cal-jamo <calvin.jameson02@gmail.com>
2025-12-03 14:54:56 -0700

    Connection class to hold the open connection with the auth token session and game ID

---
+++ server/src/main/java/webSocket/Connection.java
@@ -0,0 +1,17 @@
+package webSocket;
+import org.eclipse.jetty.websocket.api.Session;
+import java.io.IOException;
+
+public class Connection {
+    public String authToken;
+    public Session session;
+    public Integer gameId;
+    public Connection(String authToken, Session session, Integer gameId) {
+        this.session = session;
+        this.authToken = authToken;
+        this.gameId = gameId;
+    }
+    public void send(String msg) throws IOException {
+        session.getRemote().sendString(msg);
+    }
+}
