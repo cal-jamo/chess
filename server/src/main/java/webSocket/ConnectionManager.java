031e3573b3c28cf4f2384f6e152bcc61ad996f54
cal-jamo <calvin.jameson02@gmail.com>
2025-12-03 15:37:29 -0700

    Created my broadcast method

--- server/src/main/java/webSocket/ConnectionManager.java
+++ server/src/main/java/webSocket/ConnectionManager.java
@@ -1,4 +1,8 @@
 package webSocket;
+import com.google.gson.Gson;
+import websocket.messages.ServerMessage;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class ConnectionManager {
@@ -8,5 +12,22 @@ public class ConnectionManager {
     }
     public void remove(String authToken) {
         sessions.remove(authToken);
+    }
+    public void broadcast(ServerMessage notification, String authToken, Integer gameId) throws IOException {
+        var removeList = new ArrayList<String>();
+        for (var sesh : sessions.values()) {
+            if(sesh.session.isOpen()) {
+                if(sesh.gameId.equals(gameId)) {
+                    if(!sesh.authToken.equals(authToken)) {
+                        sesh.send(new Gson().toJson(notification));
+                    }
+                }
+            } else {
+                removeList.add(sesh.authToken);
+            }
+        }
+        for(var session : removeList) {
+            sessions.remove(session);
+        }
     }
 }
