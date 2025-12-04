cbfa3e36393c01476512c3986b2367cbcf9412e1
cal-jamo <calvin.jameson02@gmail.com>
2025-12-04 14:18:26 -0700

    Revert "Got dependencies to work and adding some things to my server and ws handler"
    This reverts commit 7bfd90f72d0177894230b59b2ba8eea8a0ec0f4a.

--- server/src/main/java/webSocket/WebSocketHandler.java
+++ server/src/main/java/webSocket/WebSocketHandler.java
@@ -1,11 +1,5 @@
 package webSocket;
 import com.google.gson.Gson;
-import dataaccess.auth.AuthDAO;
-import dataaccess.auth.DBAuthDAO;
-import dataaccess.game.DBGameDAO;
-import dataaccess.game.GameDAO;
-import dataaccess.user.DBUserDAO;
-import dataaccess.user.UserDAO;
 import io.javalin.websocket.WsMessageContext;
 import websocket.commands.UserGameCommand;
 import websocket.commands.MakeMoveCommand;
@@ -13,16 +7,6 @@ import websocket.commands.MakeMoveCommand;
 public class WebSocketHandler {
     private final ConnectionManager sessions =  new ConnectionManager();
     private final Gson gson = new Gson();
-    UserDAO userDAO;
-    AuthDAO authDAO;
-    GameDAO gameDAO;
-
-    public WebSocketHandler(UserDAO userDAO, AuthDAO authDAO, GameDAO gameDAO) {
-        this.userDAO = userDAO;
-        this.authDAO = authDAO;
-        this.gameDAO = gameDAO;
-    }
-
     public void handleMessage(WsMessageContext ctx) {
         String message = ctx.message();
         UserGameCommand command = gson.fromJson(message, UserGameCommand.class);
